ca2c7b363abe22297a9b119450ae22d19d31acc8
PolinaPIt <136982241+PolinaPIt@users.noreply.github.com>
2023-07-18 22:34:22 +0300

    исправления

--- script.js
+++ script.js
@@ -6,58 +6,56 @@ const probability = document.querySelector('.current-forecast p');
 const forecastItem = document.querySelector('#forecast-item');
 const forcasts = document.querySelector('.forecasts');
 
-forecastBtn.addEventListener('click', predict()) {
+forecastBtn.addEventListener('click', predict) {
 
     /* Подставляй текст нового предсказания в .current-forecast h1 */
     /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
 
-    const predictionText = `${value}`;
-    prediction.textContent = predictionText;
+    predict() {
 
-    const value = getRandomValue(6);
+        const value = getRandomValue(6);
+        const predictionText = `${value}`;
+        prediction.textContent = predictionText;
 
-    switch (value) {
-        case 0:
-            console.log('К концу текущего года твои доходы увеличатся в 3 раза.');
-            break;
-        case 1:
-            console.log('Сегодня вечером ты услышишь признание в любви от любимого человека.');
-            break;
-        case 2:
-            console.log('Твой ребенок вырастет замечательным счастливым человеком.');
-            break;
-        case 3:
-            console.log('Через 3 месяца исполнится твоя заветная мечта.');
-            break;
-        case 4:
-            console.log('Твоя работа будет тебя наполнять и вдохновлять.');
-            break;
-        case 5:
-            console.log('Тебе подарят шикарный букет.');
-            break;
-    }
+        switch (value) {
+            case 0:
+                console.log('К концу текущего года твои доходы увеличатся в 3 раза.');
+                break;
+            case 1:
+                console.log('Сегодня вечером ты услышишь признание в любви от любимого человека.');
+                break;
+            case 2:
+                console.log('Твой ребенок вырастет замечательным счастливым человеком.');
+                break;
+            case 3:
+                console.log('Через 3 месяца исполнится твоя заветная мечта.');
+                break;
+            case 4:
+                console.log('Твоя работа будет тебя наполнять и вдохновлять.');
+                break;
+            case 5:
+                console.log('Тебе подарят шикарный букет.');
+                break;
+        }
 
-    prediction.append(predictionText);
+        prediction.append(predictionText);
 
-    /* Показывай процент вероятности, с которым предсказание сбудется — в верстке это .current-forecast p */
-    /* Данный процент также нужно генерировать автоматически, он может принимать значения от 0 до 100% */
+        /* Показывай процент вероятности, с которым предсказание сбудется — в верстке это .current-forecast p */
+        /* Данный процент также нужно генерировать автоматически, он может принимать значения от 0 до 100% */
 
-    const persent = getRandomValue(101);
-    const probabilityText = `Вероятность: ${persent}%`;
-    probability.textContent = probabilityText;
+        const persent = getRandomValue(101);
+        const probabilityText = `Вероятность: ${persent}%`;
+        probability.textContent = probabilityText;
 
-    /* Совет: заведи функцию-хелпер, которая будет заниматься только генерацией данных в диапазоне от min до max и используй ее где нужно */
+        /* Совет: заведи функцию-хелпер, которая будет заниматься только генерацией данных в диапазоне от min до max и используй ее где нужно */
 
-    function getRandomValue(max) {
-        return Math.floor(Math.random() * `${max}`);
-    }
+        function getRandomValue(max) {
+            return Math.floor(Math.random() * `${max}`);
+        }
 
-    probability.append(probabilityText);
+        probability.append(probabilityText);
 
-    /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
-
-    for (let i = 0; i > 0, i++) {
-
+        /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
         /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
 
         function addInListByTemplate(predictionText, probabilityText) {
@@ -67,7 +65,8 @@ forecastBtn.addEventListener('click', predict()) {
             return list;
         }
 
-        const list = addInListByTemplate(predictionText, probabilityText);
+        const list = addInListByTemplate (predictionText, probabilityText);
         forcasts.prepend(list);
     }
-}
+
+};
