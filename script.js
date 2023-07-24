6d981c691161d6daf31100cc521fcb205c65e6cf
PolinaPIt <136982241+PolinaPIt@users.noreply.github.com>
2023-07-24 23:59:03 +0300

    исправленная версия

--- script.js
+++ script.js
@@ -6,67 +6,69 @@ const probability = document.querySelector('.current-forecast p');
 const forecastItem = document.querySelector('#forecast-item');
 const forcasts = document.querySelector('.forecasts');
 
-forecastBtn.addEventListener('click', predict) {
+forecastBtn.addEventListener('click', predict);
 
     /* Подставляй текст нового предсказания в .current-forecast h1 */
     /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
 
-    predict() {
+    function predict() {
 
         const value = getRandomValue(6);
-        const predictionText = `${value}`;
-        prediction.textContent = predictionText;
+        let predictionText;
+        let probabilityText;
 
         switch (value) {
             case 0:
-                console.log('К концу текущего года твои доходы увеличатся в 3 раза.');
-                break;
+            predictionText = 'К концу текущего года твои доходы увеличатся в 3 раза.';
+            break;
             case 1:
-                console.log('Сегодня вечером ты услышишь признание в любви от любимого человека.');
-                break;
+            predictionText = 'Сегодня вечером ты услышишь признание в любви от любимого человека.';
+            break;
             case 2:
-                console.log('Твой ребенок вырастет замечательным счастливым человеком.');
-                break;
+            predictionText = 'Твой ребенок вырастет замечательным счастливым человеком.';
+            break;
             case 3:
-                console.log('Через 3 месяца исполнится твоя заветная мечта.');
-                break;
+            predictionText = 'Через 3 месяца исполнится твоя заветная мечта.';
+            break;
             case 4:
-                console.log('Твоя работа будет тебя наполнять и вдохновлять.');
-                break;
+            predictionText = 'Твоя работа будет тебя наполнять и вдохновлять.';
+            break;
             case 5:
-                console.log('Тебе подарят шикарный букет.');
-                break;
+            predictionText = 'Тебе подарят шикарный букет.';
+            break;
         }
-
-        prediction.append(predictionText);
 
         /* Показывай процент вероятности, с которым предсказание сбудется — в верстке это .current-forecast p */
         /* Данный процент также нужно генерировать автоматически, он может принимать значения от 0 до 100% */
 
-        const persent = getRandomValue(101);
-        const probabilityText = `Вероятность: ${persent}%`;
-        probability.textContent = probabilityText;
+        if (predictionText) {
+            console.log(predictionText);
+            prediction.textContent = predictionText;
+            
+            const percent = getRandomValue(101);
+            probabilityText = `Вероятность: ${percent}%`;
+            console.log(probabilityText);
+            probability.textContent = probabilityText;
+            
+            const listItem = addInListByTemplate(predictionText, probabilityText);
+            forecasts.prepend(listItem);
+        }
 
         /* Совет: заведи функцию-хелпер, которая будет заниматься только генерацией данных в диапазоне от min до max и используй ее где нужно */
 
         function getRandomValue(max) {
-            return Math.floor(Math.random() * `${max}`);
+            return Math.floor(Math.random() * max);
         }
 
-        probability.append(probabilityText);
 
         /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
         /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
 
         function addInListByTemplate(predictionText, probabilityText) {
-            const list = forecastItem.content.cloneNode(true);
-            list.querySelector('.forecast-item h3').textContent = predictionText;
-            list.querySelector('.forecast-item p').textContent = probabilityText;
-            return list;
+            const listItem = forecastItem.content.cloneNode(true);
+            listItem.querySelector('.forecast-item h3').textContent = predictionText;
+            listItem.querySelector('.forecast-item p').textContent = probabilityText;
+            return listItem;
         }
-
-        const list = addInListByTemplate (predictionText, probabilityText);
-        forcasts.prepend(list);
     }
 
-};
